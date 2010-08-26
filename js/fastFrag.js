1a77041d8c640d9859d1bf36e23b42d3ea11cb81
gregory80 <gregory.tomlinson@gmail.com>
2010-08-25 20:36:34 -0400

    updates based on testing in chrome, usage and complexity, pretty fing fast

--- js/fastFrag.js
+++ js/fastFrag.js
@@ -11,17 +11,17 @@
             usage
 
             // element 'type' is always assumed to be a div unless otherwise noted via 'type' attr
-            fastFrag.create([{
+            fastFrag.create({
                 css : "a_class_name",
                 content : "string here"
-            }])
+            })
             content is REQUIRED, set to empty string or element is skipped (when === undefined )
 
             //returns <div class="a_class_name">string here</div>
 
             Get more complex with with nested elements:
 
-            fastFrag.create([{
+            fastFrag.create({
                 css : "a_class_name",
                 content : [{
                     type : "a"
@@ -32,7 +32,7 @@
                 }, {
                     content : "child two"
                 }]
-            }])   
+            })   
 
             // returns   
             <div class="a_class_name">
@@ -73,30 +73,52 @@
         // also called escaper, breaking it out.
         return (string && string.replace(reg1, "&amp;").replace(reg2, "&quot;").replace(reg3, "&#39;")
                      .replace(reg4, "&gt;").replace(reg5, "&lt;") ) || "";
-    }    
+    } 
+    function _singleNode( o ) {
+
+        var frag = d.createDocumentFragment(), el, el_name, content_type, txt;
+        //if(o.content === undefined) { return frag; }
+        content_type = typeof o.content;            
+        //console.log("single node", o)
+        el_name = ( safe_el.indexOf( o.type ) > -1 ) ? o.type : "div";
+        el = _mke( el_name );
+        _mke_attribute(el, o.attributes);
+        el.id = (o.id) ? o.id : null;
+        el.className = (o.css) ? o.css : null;
+        
+        txt=null;    
+        if( content_type === "object") {
+            txt = drawHTML( o.content );
+        } else if(content_type === "string") {
+            txt = d.createTextNode( _safe( o.content ) );
+        } else if(o.content === undefined) {
+            // todo, fix this, it's weird
+            // messy
+            frag.appendChild(el);              
+            return frag;
+        } else {
+            txt = d.createTextNode( _safe( o.content.toString() ) );
+        }
+        el.appendChild( txt );  
+        frag.appendChild(el);  
+
+        return frag;
+    }   
     function drawHTML( params ) {        
-        var el_name, frag = d.createDocumentFragment(), k, el, txt, o, key, content_type;        
-        for(k in params) {
-            o = params[k];
-            if(o.content === undefined) { continue; }
-            content_type = typeof o.content;            
-            
-            el_name = ( safe_el.indexOf( o.type ) > -1 ) ? o.type : "div";
-            el = _mke( el_name );
-            _mke_attribute(el, o.attributes);
-            el.id = (o.id) ? o.id : null;
-            el.className = (o.css) ? o.css : null;
-            
-            txt=null;    
-            if( content_type === "object") {
-                txt = drawHTML( o.content );
-            } else if(content_type === "string") {
-                txt = d.createTextNode( _safe( o.content ) );
-            } else {
-                txt = d.createTextNode( _safe( o.content.toString() ) )
+        var frag = d.createDocumentFragment(), k, el;   
+        //todo
+        // fix this issue, it should know where to loop or not, i could probably type check on params
+        //console.log(typeof params, params.length,  params)     
+        if(params.length === undefined) {
+            //console.log("single", params)
+            el = _singleNode( params );
+            frag.appendChild( el );
+        } else {
+            //console.log('loopin')
+            for(k in params) {
+                el = _singleNode( params[k] );
+                frag.appendChild( el );              
             }
-            el.appendChild( txt );  
-            frag.appendChild(el);
         }
         return frag;
     }    
