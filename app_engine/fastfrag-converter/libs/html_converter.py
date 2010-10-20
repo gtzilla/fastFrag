0c4f7ccf99dcccaec8c46d17ed00decd0ada3e6d
gregory80 <gregory.tomlinson@gmail.com>
2010-10-20 00:30:40 -0400

    messing with the convertor, logic is flawed

--- app_engine/fastfrag-converter/libs/html_converter.py
+++ app_engine/fastfrag-converter/libs/html_converter.py
@@ -30,14 +30,16 @@ class FastFragHTMLParser(HTMLParser):
     
     def handle_startendtag(self, tag, attrs):
 
-        frag = self._create_basic_frag( tag, attrs )        
+        frag = self._create_basic_frag( tag, attrs )
         
         logging.info("start_endtag %s and  %s" % (tag, frag) )        
         ## decrement self close
         ## no action for sel closed?
         
     def handle_data(self,data):
-        logging.info("data? is this the text? %s" % data )
+        if data and data.strip() != "":
+            logging.info("is this the text? %s for frag %s" % (data, self.active_frag ) )
+            #self.active_frag['content'] = "%s" % data
     
     def handle_starttag(self, tag, attrs):
         
@@ -47,23 +49,63 @@ class FastFragHTMLParser(HTMLParser):
         
         self.node_depth+=1
         # fast frag assumes div, skip type for this element
-        frag = self._create_basic_frag( tag, attrs )        
+        frag = self._create_basic_frag( tag, attrs ) 
+        self.active_frag=frag       
         if not self.fragList:
             self.fragList = frag
-        else:
+            self.fragList['content'] = {}
+        else:    
             self._get_frag_location( frag, self.fragList )
+        # else:
+        #     self._get_frag_location( frag, self.fragList )
             
     def _get_frag_location(self, frag, start_location):
-        logging.info("thing type is %s" % type(start_location) )
-        logging.info(self.getpos() )
-        if start_location.get('content'):
-            self._get_frag_location( frag, start_location.get('content') )
-        else:
-            start_location['content'] = frag
-            
-            
-            
-    
+        #logging.info("thing type is %s" % type(start_location) )
+
+        self.counter=0
+        self._last_elem=None
+        def lookup( frag_el, start_el ):
+            logging.info("goingdeep  to %s and %s depth %d" %  ( frag_el, start_el, self.node_depth  )  )
+            self.counter+=1
+            if self.counter == self.node_depth:
+                logging.info("depth math %s" % frag_el)
+            else:
+                try:
+                    next_el = start_el.get("content")
+                except Exception, msg:
+                    logging.info("hehhe %s" % msg )
+                    pass
+                self._last_elem=start_el
+                lookup( frag_el, next_el)
+            # #logging.info(" elem type %s for frag %s" % (type(start_el), frag_el ) )
+            # next_el = start_el and start_el.get('content')
+            # #logging.info("frag %s  start %s next %s" % ( frag_el, start_el, next_el ) )
+            # if self.node_depth < self.counter:
+            #     logging.info("goingdeep  to %s and %s next el %s" %  ( frag_el, start_el, next_el  )  )
+            #     self.counter+=1                 
+            #     lookup( frag_el, next_el )
+            # else:
+            #     if start_el and self.node_depth == self.counter:
+            #         logging.info("start...")
+            #         start_el['content'] = frag_el
+            #     elif next_el:
+            #         if type(next_el) == dict:
+            #             start_el['content'] = [next_el, frag_el]
+            #         elif type(next_el) == list:
+            #             logging.info("its a list now")
+            #             next_el.append(frag_el)
+            # 
+            #             
+            #         logging.info("weird case to %s and %s next el %s" % ( frag_el, start_el, next_el  ) )
+            # 
+            #     logging.info("all to %s and %s next el %s" % ( frag_el, start_el, next_el  ) )
+            # 
+            # 
+            # return start_el
+        
+        lookup( frag, start_location )
+
+
     def handle_endtag(self, tag):
         # print self.fragList
         self.node_depth-=1
@@ -74,6 +116,7 @@ class FastFragHTMLParser(HTMLParser):
     
     def __init__(self):
         self.fragList=None
+        self.active_frag=None
         self.attach_target=None
         self.node_depth=0
         self.allFrags=[]
